c4a9ecca250632c32fcc0e99914be3f5efec9d58
junhwanbag460-pixel <junhwanbag460@gmail.com>
2026-03-15 13:44:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,66 @@
-const API="https://script.google.com/macros/s/AKfycbzf85dqYjE17WJv2gZP_FNqt8AQPp5kUDQwnSs2cMbPJac2Y2BXKpQBatGVm8ai6TWG/exec"
+const API = "https://script.google.com/macros/s/AKfycbzf85dqYjE17WJv2gZP_FNqt8AQPp5kUDQwnSs2cMbPJac2Y2BXKpQBatGVm8ai6TWG/exec"
 
-function calculate(gpa,language,score){
 
-const gpaScore=(gpa/4.3)*50
 
-let lang=0
+function calculate(gpa, language, score){
 
-if(language=="TOEFL") lang=(score/120)*40
-if(language=="IELTS") lang=(score/9)*40
-if(language=="ITP") lang=(score/677)*40
+const gpaScore = (gpa / 4.3) * 50
 
-return gpaScore+lang+10
+let langScore = 0
+
+if(language == "TOEFL"){
+langScore = (score / 120) * 40
+}
+
+if(language == "IELTS"){
+langScore = (score / 9) * 40
+}
+
+if(language == "ITP"){
+langScore = (score / 677) * 40
+}
+
+return gpaScore + langScore + 10
 
 }
 
 
 
-document.getElementById("form")?.addEventListener("submit",async(e)=>{
+document.getElementById("form").addEventListener("submit", async function(e){
 
 e.preventDefault()
 
-const email=localStorage.getItem("email")
+const email = localStorage.getItem("email")
 
-const gpa=parseFloat(document.getElementById("gpa").value)
-const language=document.getElementById("language").value
-const score=parseFloat(document.getElementById("score").value)
+const name = document.getElementById("name").value
+const gpa = parseFloat(document.getElementById("gpa").value)
+const language = document.getElementById("language").value
+const score = parseFloat(document.getElementById("score").value)
 
-const total=calculate(gpa,language,score)
+const total = calculate(gpa, language, score)
 
-const data={
+const data = {
 
-email:email,
-name:document.getElementById("name").value,
-gpa:gpa,
-language:language,
-score:score,
-total:total,
+email: email,
+name: name,
+gpa: gpa,
+language: language,
+score: score,
+total: total,
 
-choice1:document.getElementById("c1").value,
-choice2:document.getElementById("c2").value,
-choice3:document.getElementById("c3").value,
-choice4:document.getElementById("c4").value,
-choice5:document.getElementById("c5").value
+choice1: document.getElementById("c1").value,
+choice2: document.getElementById("c2").value,
+choice3: document.getElementById("c3").value,
+choice4: document.getElementById("c4").value,
+choice5: document.getElementById("c5").value
 
 }
 
 await fetch(API,{
-method:"POST",
-body:JSON.stringify(data)
+method: "POST",
+body: JSON.stringify(data)
 })
 
-window.location.href="result.html"
+window.location.href = "result.html"
 
 })
